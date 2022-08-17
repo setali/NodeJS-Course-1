d83b7c92628eead0d66f32304cbd5340cff2969c
SAli <set.ali.mousavi@gmail.com>
2022-08-17 20:34:42 +0430

    5 - custom render

--- blog/src/app.js
+++ blog/src/app.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import router from './routes'
 import errorHandler from './middleware/error-handler'
+import renderTemplate from './middleware/render-template'
+
+global.__basedir = __dirname
 
 const app = express()
 
 app.use(express.static('public'))
+
+app.use(renderTemplate)
 
 const port = 8000
 
--- blog/src/controllers/general.js
+++ blog/src/controllers/general.js
@@ -1,14 +1,28 @@
-import path from 'path'
-
 export function home (req, res) {
-  const filePath = path.resolve(__dirname, '..', 'views', 'index.html')
-  res.sendFile(filePath)
+  // const filePath = path.resolve(__dirname, '..', 'views', 'index.html')
+
+  // const file = fs.readFileSync(filePath, 'utf-8')
+
+  // const view = file
+  //   .replace('#title#', 'Home page')
+  //   .replace('#message#', 'This is homepage')
+
+  res.renderTemplate('index', {
+    title: 'Home Page',
+    message: 'This is homepage'
+  })
 }
 
 export function about (req, res) {
-  res.send('About page')
+  res.renderTemplate('index', {
+    title: 'About us',
+    message: 'This is About page'
+  })
 }
 
 export function contact (req, res) {
-  res.send('Contact page')
+  res.renderTemplate('index', {
+    title: 'Contact us',
+    message: 'This is Contact page'
+  })
 }
