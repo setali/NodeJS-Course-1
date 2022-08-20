87ac43433e2f3e49b0bdcd1a7ac503adc21fbe67
SAli <set.ali.mousavi@gmail.com>
2022-08-20 17:37:40 +0430

    7 - pug

--- blog/src/app.js
+++ blog/src/app.js
@@ -2,24 +2,11 @@ import express from 'express'
 import router from './routes'
 import errorHandler from './middleware/error-handler'
 import path from 'path'
-import fs from 'fs'
 
 const app = express()
 
-app.engine('ali', (filePath, params, callback) => {
-  let view = fs.readFileSync(filePath, 'utf-8')
-
-  Object.entries(params).forEach(([key, value]) => {
-    if (typeof value === 'string') {
-      view = view.replace(`#${key}#`, value)
-    }
-  })
-
-  return callback(null, view)
-})
-
 app.set('views', path.resolve(__dirname, 'views'))
-app.set('view engine', 'ali')
+app.set('view engine', 'pug')
 
 app.use(express.static('public'))
 
